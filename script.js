6e0e54157697560ab1b89b1ac27fbf948b6ef966
Nuryana N <nuryan504@gmail.com>
2023-02-07 08:47:28 +0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+var swiper = new Swiper(".mySwiperFriend", {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
